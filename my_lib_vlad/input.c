f2e98a8bac7b86bb0e9bcc8c8735108f019e7e2e
Vlad <Ss7532159@yandex.ru>
2021-08-09 13:44:00 +0300

    ya nich ne sdelal no sdelal biblioteki

---
+++ my_lib_vlad/input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "input.h"
+
+
+void Saycookie(int count)
+{
+    if (count == 1)
+    {
+        printf("i wanna eat cookie\n");
+    }
+    else 
+    {
+        printf("i do not wanna eat cookie\n");
+    }
+
+}
