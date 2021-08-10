5f907d570ff3dbf863694a9b173c957203e4a744
PetrPovt <petr.povt@gmail.com>
2021-08-10 10:24:05 +0300

    he want biscuit

---
+++ ThisPP/biscuit/want.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void IWantCookes(){
+    int count = 0;
+    cin >> count;
+    for(int i = 0; i < count; i++){
+        cout << "i want biscuit/n";
+    }
+}
--- ThisPP/main.c
+++ ThisPP/main.c
@@ -1,7 +1,6 @@
-#include <iostream>
-using namespace std;
+#include "biscuit/wont.h"
 
 int main(){
-    cin >> int a
-    IWantCookes(a)
+    IWantCookes();
+    return 0;
 }
